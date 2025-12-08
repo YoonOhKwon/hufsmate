050b494c789d6b6f292d0875f56bf74f9c475976
YoonOhKwon <yoonojio11@gmail.com>
2025-12-09 03:11:18 +0900

    Update crolling.py

--- crolling.py
+++ crolling.py
@@ -3,22 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import time
+import json
 
 def crawl_all_notices(user_id, user_pw):
 
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=chrome_options)
     wait = WebDriverWait(driver, 10)
 
-    # -------------------------------
-    # 로그인
-    # -------------------------------
     driver.get("https://wis.hufs.ac.kr/src08/jsp/twofactor_login.jsp")
 
     wait.until(EC.presence_of_element_located((By.NAME, "user_id")))
@@ -26,79 +20,59 @@ def crawl_all_notices(user_id, user_pw):
     driver.find_element(By.ID, "password").send_keys(user_pw)
     driver.find_element(By.ID, "login_btn").click()
 
-    # -------------------------------
-    # 강의 목록 불러오기
-    # -------------------------------
     wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "em.sub_open")))
     courses = driver.find_elements(By.CSS_SELECTOR, "em.sub_open")
     course_titles = [c.get_attribute("title") for c in courses]
 
-    notice_title_list = []
-    notice_content_list = []
+    notice_titles = []
+    notice_contents = []
 
-    # -------------------------------
-    # 강의별 공지 크롤링
-    # -------------------------------
     for i in range(len(course_titles)):
-
-        # 메인 페이지 접속
         driver.get("https://eclass.hufs.ac.kr/ilos/main/main_form.acl")
         wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "em.sub_open")))
 
         courses = driver.find_elements(By.CSS_SELECTOR, "em.sub_open")
-
-        # 강의 클릭
-        wait.until(EC.element_to_be_clickable(courses[i]))
         driver.execute_script("arguments[0].click();", courses[i])
 
-        # 공지사항 메뉴 클릭
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='공지사항']")))
-        notice_btn = driver.find_element(By.CSS_SELECTOR, "img[alt='공지사항']")
-        driver.execute_script("arguments[0].click();", notice_btn)
+        driver.find_element(By.CSS_SELECTOR, "img[alt='공지사항']").click()
 
-        # 공지 목록 불러오기
         wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "tr.list, tr[style*='cursor: pointer']")))
         notices = driver.find_elements(By.CSS_SELECTOR, "tr.list, tr[style*='cursor: pointer']")
 
-        lecture_notice_titles = []
-        lecture_notice_contents = []
+        lecture_titles = []
+        lecture_contents = []
 
         for j in range(len(notices)):
-
             notices = driver.find_elements(By.CSS_SELECTOR, "tr.list, tr[style*='cursor: pointer']")
 
-            # 제목
             try:
                 title = notices[j].find_element(By.CSS_SELECTOR, "div.subjt_top").text.strip()
             except:
                 title = "(제목 없음)"
 
-            lecture_notice_titles.append(title)
+            lecture_titles.append(title)
 
-            # 공지 클릭
-            link = notices[j].find_element(By.CSS_SELECTOR, "a.site-link")
-            driver.execute_script("arguments[0].click();", link)
-
-            # 본문 로드
+            driver.execute_script("arguments[0].click();", notices[j].find_element(By.CSS_SELECTOR, "a.site-link"))
             wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "td.textviewer")))
+
             try:
                 content = driver.find_element(By.CSS_SELECTOR, "td.textviewer").text.strip()
             except:
                 content = "(내용 없음)"
 
-            lecture_notice_contents.append(content)
-
-            # 뒤로
+            lecture_contents.append(content)
             driver.back()
-            wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "tr.list, tr[style*='cursor: pointer']")))
 
-        notice_title_list.append(lecture_notice_titles)
-        notice_content_list.append(lecture_notice_contents)
+        notice_titles.append(lecture_titles)
+        notice_contents.append(lecture_contents)
 
-    # 종료
     driver.quit()
 
-    # -------------------------------
-    # 최종 반환
-    # -------------------------------
-    return notice_title_list, notice_content_list, course_titles
+    with open("cache_titles.json", "w", encoding="utf-8") as f:
+        json.dump(notice_titles, f, ensure_ascii=False, indent=2)
+
+    with open("cache_contents.json", "w", encoding="utf-8") as f:
+        json.dump(notice_contents, f, ensure_ascii=False, indent=2)
+
+    return notice_titles, notice_contents, course_titles
